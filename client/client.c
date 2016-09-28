c93a3eeee8326935f24d8b70985322c1a9bda094
John Rocha <john.rocha5@gmail.com>
2016-09-28 18:21:03 -0400

    Initial file commit

---
+++ client/client.c
@@ -0,0 +1,3 @@
+// client.c
+// John Rocha, Mary Connolly, Paul Dowling
+// jrocha2, mconnol6, pdowling
---
+++ server/server.c
@@ -0,0 +1,3 @@
+// server.c
+// John Rocha, Mary Connolly, Paul Dowling
+// jrocha2, mconnol6, pdowling
